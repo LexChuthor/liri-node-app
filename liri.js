9210799427e57bc01684299c9dcbc2ffd5833525
Lex Chuthor <psc9qb@gmail.com>
2018-10-14 19:27:51 -0400

    added logging of inputs

--- liri.js
+++ liri.js
@@ -2,45 +2,67 @@ require("dotenv").config();
 var request = require("request");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
-var query = process.argv[2];
-var term = toLowerCase(process.argv.splice(3).join(" "));
+var query = process.argv[2].toLocaleLowerCase();
+var term = process.argv.splice(3).join(" ").toLowerCase();
 
-if (query === "concert-this") {
 
-    var URL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
-    request(URL, function(error, response, data){
-        if (!error && response.statusCode === 200){
-            var jsonDATA = JSON.parse(data);
-            console.log(jsonDATA);
+liriBot(query, term);
+
+function liriBot(query, term){
+    if (query === "concert-this") {
+        concertSearch(term);
+    
+    } else if (query === "spotify-this-song") {
+        if (term === "") {
+            term = "The Sign ace of base";
         }
-    });
-
-
-} else if (query === "spotify-this-song") {
-    if (term === "") {
-        term = "The Sign ace of base";
+        spotSearch(term);
+    
+    } else if (query === "movie-this") {
+        if(term === ""){
+            term = "Mr. Nobody";
+        }
+        movieSearch(term);
+    
+    } else if (query === "do-what-it-says") {
+        fs.readFile("random.txt", "utf8", function(error, data){
+            if(error){
+                return console.log(error);
+            }
+            console.log(data);
+            var dataArray = data.split(",");
+            query = dataArray[0];
+            term = dataArray[1];
+            liriBot(query, term);
+        });
     }
+}
+
+function spotSearch(term){
+
     spotify.search({ type: "track", query: term, limit: 1 }, function (error, data) {
         if (error) {
             return console.log(error);
         } else {
             //    console.log(data.tracks.items[0].artists);
-            console.log(`
+            var output = (`
             Artist(s): ${data.tracks.items[0].artists.map(artist => artist.name).join(", ")}
             Song Name: ${data.tracks.items[0].name}
             Preview: ${data.tracks.items[0].preview_url}
             Album: ${data.tracks.items[0].album.name}
             `);
+            console.log(output);
+            logFile(output);
         }
 
     });
+}
 
-} else if (query === "movie-this") {
-    if(term === ""){
-        term = "Mr. Nobody";
-    }
+function movieSearch(term){
     var URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + term;
     request(URL, function (error, response, data) {
         if (!error && response.statusCode === 200) {
@@ -59,7 +81,7 @@ if (query === "concert-this") {
                 }
             }
 
-            console.log(`
+            var output = (`
             Title: ${jsonDATA.Title}
             Year: ${jsonDATA.Year}
             IMDB Rating: ${imdbRating}
@@ -68,10 +90,39 @@ if (query === "concert-this") {
             Language: ${jsonDATA.Language}
             Plot Summary: ${jsonDATA.Plot}
             Actors: ${jsonDATA.Actors}
-            `)
+            `);
+            logFile(output);
+            console.log(output);
         }
     })
+}
 
-} else if (query === "do-what-it-says") {
+function concertSearch(term){
+    var URL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
+    request(URL, function(error, response, data){
+        if (!error && response.statusCode === 200){
+            var jsonDATA = JSON.parse(data);
+            //console.log(jsonDATA);
+            for(var i = 0; i < jsonDATA.length; i++){
+                var output = (`===============
+                               ${jsonDATA[i].venue.name}
+                               ${moment(jsonDATA[i].datetime).format("MM/DD/YY")}
+                               ===============`);
+               console.log(output);
+               logFile(output);
+            }
+        }
+    });
+}
 
+function logFile(text){
+    text = "\n" + "====================" + query + text + "\n" + "====================";
+    fs.appendFile("log.txt", text, function(error){
+        if(error){
+            return console.log(error);
+        }
+        else {
+            console.log("Content Added");
+        }
+    })
 }
